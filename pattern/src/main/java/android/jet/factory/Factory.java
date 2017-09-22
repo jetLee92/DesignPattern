e98f6e9a97666a2fc9bb76322e140155076b59b1
mengmengda jet <940077208@qq.com>
2017-09-22 17:32:00 +0800

    提交单例和工厂方法

---
+++ pattern/src/main/java/android/jet/factory/Factory.java
@@ -0,0 +1,14 @@
+package android.jet.factory;
+
+/**
+ * @author liqisi
+ * @description：
+ * @date：2017/9/22
+ */
+public class Factory {
+
+	public static void main(String[] args) {
+		System.out.print("测试222");
+	}
+
+}
---
+++ pattern/src/main/java/android/jet/singleton/Singleton.java
@@ -0,0 +1,9 @@
+package android.jet.singleton;
+
+public class Singleton {
+
+	public static void main(String[] args) {
+		System.out.print("测试");
+	}
+
+}
